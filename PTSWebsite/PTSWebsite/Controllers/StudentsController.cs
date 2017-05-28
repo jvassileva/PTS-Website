ab76fe772c1e36e89f0a0c4c7d574e8b8dd47537
Ivo <ikaraneshev95@gmail.com>
2017-05-28 23:58:43 +0300

    Add sample students section

---
+++ PTSWebsite/PTSWebsite/Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PTSWebsite.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Scholarships()
+        {
+            return View();
+        }
+
+        public ActionResult Dormitories()
+        {
+            return View();
+        }
+    }
+}
